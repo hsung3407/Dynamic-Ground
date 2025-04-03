8f80ac51255c40ba5ac9dda68d5ba0b17830c581
hsung3407 <azxccxzavv3407@gmail.com>
2025-04-03 13:44:31 +0900

    Initial commit

---
+++ Assets/Script/Networking/Networking.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Networking : MonoBehaviour
+{
+    //groq ai api key
+    private const string APIKey = "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";
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
