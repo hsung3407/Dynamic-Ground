4d057002cefc025dee9f202315cfd643e199906e
hsung3407 <azxccxzavv3407@gmail.com>
2025-04-03 19:19:43 +0900

    Update PlayerPrompt.cs

--- Assets/Script/PlayerPrompt.cs
+++ Assets/Script/PlayerPrompt.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Script
@@ -15,18 +16,15 @@ namespace Script
         [SerializeField] private int heightExpendLimit = 5;
         private float _defaultHeight;
 
-        private GameManager _gameManager;
-        
+        [SerializeField] private GameManager gameManager;
+
         private void Start()
         {
-            _gameManager = FindObjectOfType<GameManager>();
-            
             _playerPromptRect = playerPrompt.GetComponent<RectTransform>();
             _defaultHeight = _playerPromptRect.sizeDelta.y;
             playerPrompt.onValueChanged.RemoveAllListeners();
             playerPrompt.onValueChanged.AddListener(s =>
             {
-                Debug.Log("Changed");
                 var size = _playerPromptRect.sizeDelta;
                 size.y = _defaultHeight * Mathf.Min(Regex.Matches(s, "\n").Count + 1, Mathf.Max(heightExpendLimit, 1));
                 _playerPromptRect.sizeDelta = size;
@@ -38,7 +36,7 @@ namespace Script
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 var input = playerPrompt.text;
-                if (Input.GetKey(KeyCode.LeftShift) || input.Trim() == "" || _gameManager.InputPrompt(input)) return;
+                if (Input.GetKey(KeyCode.LeftShift) || input.Trim() == "" || gameManager.InputPrompt(input)) return;
                 playerPrompt.text = "";
             }
         }
