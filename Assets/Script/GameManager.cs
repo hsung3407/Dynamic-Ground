0e97d39933656213201b4c7298853c4505b5c2f6
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-19 15:10:07 +0900

    update: game manager
    로그 적용

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,13 +6,23 @@ namespace Script
     {
         private bool _isWaitResponse;
         
+        [SerializeField] private LogManager logManager;
+
         public bool InputPrompt(string prompt)
         {
             if (_isWaitResponse) return false;
-
+            _isWaitResponse = true;
             
             
+            
+            logManager.SetLog(prompt, true);
             return true;
+        }
+
+        private void ReceiveResponse(string response)
+        {
+            _isWaitResponse = false;
+            logManager.SetLog(response, false);
         }
     }
 }
