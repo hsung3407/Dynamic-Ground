83b4af9e8c0ab3d6145826fe31813260fc4e3504
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-19 19:59:30 +0900

    update: player prompt
    1. Shift시에 줄 바꿈 선택 기능 제거
    2. 텍스트 줄 인식하여 영역 조절 기능 개선

--- Assets/Script/PlayerPrompt.cs
+++ Assets/Script/PlayerPrompt.cs
@@ -18,17 +18,14 @@ namespace Script
 
         [SerializeField] private GameManager gameManager;
 
+        private bool _refreshFlag;
+
         private void Start()
         {
             _playerPromptRect = playerPrompt.GetComponent<RectTransform>();
             _defaultHeight = _playerPromptRect.sizeDelta.y;
             playerPrompt.onValueChanged.RemoveAllListeners();
-            playerPrompt.onValueChanged.AddListener(s =>
-            {
-                var size = _playerPromptRect.sizeDelta;
-                size.y = _defaultHeight * Mathf.Min(Regex.Matches(s, "\n").Count + 1, Mathf.Max(heightExpendLimit, 1));
-                _playerPromptRect.sizeDelta = size;
-            });
+            playerPrompt.onValueChanged.AddListener(_ => { _refreshFlag = true; });
         }
 
         private void Update()
@@ -36,13 +33,26 @@ namespace Script
             if (Input.GetKeyDown(KeyCode.Return))
             {
                 var input = playerPrompt.text;
-                if (Input.GetKey(KeyCode.LeftShift))
-                {
-                    playerPrompt.text = input + "\n";
-                    return;
-                }
                 if (input.Trim() == "" || !gameManager.InputPrompt(input)) return;
                 playerPrompt.text = "";
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (_refreshFlag)
+            {
+                _refreshFlag = false;
+
+                var size = _playerPromptRect.sizeDelta;
+                var max = _defaultHeight * heightExpendLimit;
+                var preferredHeight = playerPrompt.preferredHeight;
+                var difference = preferredHeight % _defaultHeight;
+                var dynamicalHeight = preferredHeight % _defaultHeight > _defaultHeight / 2
+                    ? preferredHeight + difference
+                    : preferredHeight - difference;
+                size.y = Mathf.Clamp(dynamicalHeight, _defaultHeight, max);
+                _playerPromptRect.sizeDelta = size;
             }
         }
     }
