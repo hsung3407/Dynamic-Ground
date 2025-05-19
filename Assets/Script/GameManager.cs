75c298805430fbbe2bfae7c9c578d392c9ab08ea
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-19 18:38:51 +0900

    update: game manager
    model 적용

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,3 +1,4 @@
+using Script.Networking;
 using UnityEngine;
 
 namespace Script
@@ -5,24 +6,25 @@ namespace Script
     public class GameManager : MonoBehaviour
     {
         private bool _isWaitResponse;
-        
+
         [SerializeField] private LogManager logManager;
+        [SerializeField] private Model model;
 
         public bool InputPrompt(string prompt)
         {
             if (_isWaitResponse) return false;
             _isWaitResponse = true;
-            
-            
-            
+
+            model.Chat(prompt, ReceiveResponse);
+
             logManager.SetLog(prompt, true);
             return true;
         }
 
-        private void ReceiveResponse(string response)
+        private void ReceiveResponse(Schemas.ChatResponse response)
         {
             _isWaitResponse = false;
-            logManager.SetLog(response, false);
+            logManager.SetLog(response.choices[0].message.content, false);
         }
     }
 }
