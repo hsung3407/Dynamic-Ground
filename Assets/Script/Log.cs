0245461e00d0dd26791fb361ab5550aeb444fe43
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-19 19:23:03 +0900

    fix: text restrictor
    의도된 바와 다른 적용 수정 및 개선

--- Assets/Script/Log.cs
+++ Assets/Script/Log.cs
@@ -5,11 +5,20 @@ using UnityEngine.UI;
 public class Log : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI tmp;
+    [SerializeField] private float restrictWidth = 0;
     
     public void SetLog(string text)
     {
         tmp.text = text;
         var rect = GetComponent<RectTransform>();
+        
+        if(restrictWidth != 0) Restrict();
+        
         LayoutRebuilder.ForceRebuildLayoutImmediate(rect);
+    }
+
+    public void Restrict()
+    {
+        tmp.rectTransform.sizeDelta = new Vector2(Mathf.Min(800, tmp.preferredWidth + 20), tmp.rectTransform.sizeDelta.y);
     }
 }
