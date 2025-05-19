cb3407fa683a44ba90ff62155e62e3bda6646f1f
hsung3407 <azxccxzavv3407@gmail.com>
2025-05-19 16:55:59 +0900

    update: schemas

--- Assets/Script/Networking/Schemas.cs
+++ Assets/Script/Networking/Schemas.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Script.Networking
 {
@@ -15,20 +17,57 @@ namespace Script.Networking
         public class ChatRequest
         {
             public string model;
-            public Messages messages;
+            public List<Message> messages;
         }
 
         [Serializable]
-        public class Messages
-        {
-            public Message[] messages;
-        }
-        
-        [Serializable]
+        [JsonObject("massage")]
         public class Message
         {
             public string role;
             public string content;
+        }
+
+        [Serializable]
+        public class ChatResponse
+        {
+            public string id;
+            [JsonProperty("object")] public string obj;
+            public long created;
+            public string model;
+            public List<Choice> choices;
+            public Usage usage;
+            public string system_fingerprint;
+            public XGroq x_groq;
+        }
+
+        [Serializable]
+        [JsonObject("usage")]
+        public class Usage
+        {
+            public double queue_ime;
+            public long prompt_tokens;
+            public double prompt_time;
+            public long completion_tokens;
+            public double completion_time;
+            public long total_tokens;
+            public double total_time;
+        }
+
+        [Serializable]
+        public class Choice
+        {
+            public long index;
+            public Message message;
+            public string logprobs;
+            public string finish_reason;
+        }
+
+        [Serializable]
+        [JsonObject("x_groq")]
+        public class XGroq
+        {
+            public string id;
         }
     }
 }
