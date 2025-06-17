84cf7a23788ca676b1e31c88c0b49060deb2be1b
hsung3407 <azxccxzavv3407@gmail.com>
2025-06-17 21:24:56 +0900

    Update Model.cs

--- Assets/Script/Model.cs
+++ Assets/Script/Model.cs
@@ -32,20 +32,6 @@ namespace Script
                         role = "system",
                         content = "너는 이 메시지 내용에 있는 system 컨텍스트와 예시 상호작용 대화 및 실제 상호작용 대화를 보고 의도된 분위기와 형식의 답변을 해야한다."
                     });
-
-                messages.messages.Insert(3,
-                    new Schemas.Message()
-                    {
-                        role = "system",
-                        content =
-                            "출력: [2~6]의 출력 형식의 경우 무조건 특정 행동만을 할 수 있으며, 그 행동에 운 요소가 포함되거나 성공과 실패로 판가름 할 수 있을 경우에만 붙일 수 있다는 것을 명심하라"
-                    });
-                messages.messages.Insert(4,
-                    new Schemas.Message()
-                    {
-                        role = "system",
-                        content = "출력: [2~6]이 붙은 출력은 최대한 적게 나오도록 조절해야된다."
-                    });
                 _preSettingData.Add(messages);
             }
             // PreSetting();
@@ -64,12 +50,6 @@ namespace Script
             var reqMessages = _preSettingData[Random.Range(0, _preSettingData.Count)].messages;
             reqMessages.AddRange(_talkLogData);
 
-            reqMessages.Add(new Schemas.Message()
-            {
-                role = "user",
-                content = "[2~6]이 20% 확률로 나오도록 해라"
-            });
-
             var req = new Schemas.ChatRequest();
             req.model = model;
             req.messages = reqMessages;
@@ -78,7 +58,7 @@ namespace Script
             API.Chat(req)
                 .OnResponse(onResponse + (res =>
                 {
-                    _talkLogData.Add(res.choices[0].message);
+                    _talkLogData.Add(res.choices[Random.Range(0, res.choices.Count)].message);
                     IsWait = false;
                 }))
                 .Build();
